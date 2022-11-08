44ab908cd18917d3be3466e69c3d736aab578456
Eagle <liuzh@igoldenbeta.com>
2022-11-08 13:32:04 +0800

    中信建投改版-重新采集

--- data/ms/securities/zxjt_securities_collect.py
+++ data/ms/securities/zxjt_securities_collect.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # author yanpan
 # 2022/06/29 16:47
-
+import math
 import os
 import sys
 import json
@@ -19,15 +19,24 @@ class CollectHandler(BaseHandler):
         super().__init__()
         self.mq_msg = os.path.basename(__file__).split('.')[0]
         self.data_source = '中信建投'
-        self.url = 'https://www.csc108.com/kyrz/rzrqList.json'
+        self.url = 'https://www.csc108.com/marginTrading'
+        self.url = 'https://wwwxchw.csc108.com/server/siteindex/margin/getRzrqList?stkcode=&market=&type=&fundctrlflag=&stkctrlflag=&current=1&size=10000'
 
     def guaranty_and_underlying_securities_collect(self):
-        params = {'curPage': 1}
-        response = self.get_response(self.url, 0, get_headers(), params)
-        text = json.loads(response.text)
-        self.tmp_df = pd.DataFrame(text['list'])
-        self.total_num = int(text['totalCount'])
-        self.collect_num = self.tmp_df.index.size
+        pages = math.inf
+        size = 10000
+        url = self.url
+        while pages:
+            response = self.get_response(url, 0, get_headers())
+            data = json.loads(response.text)['data']
+            if pages == math.inf:
+                self.total_num = int(data['total'])
+                pages = int(data['pages'])
+                size = int(data['size'])
+            pages -= 1
+            url = self.url.replace('current=1', f"current={data['pages'] -pages + 1}").replace('size=10000', f'size={size}')
+            self.tmp_df = pd.concat([self.tmp_df, pd.DataFrame(data['records'])])
+            self.collect_num = self.tmp_df.index.size
         self.data_text = self.tmp_df.to_csv(index=False)
 
 
