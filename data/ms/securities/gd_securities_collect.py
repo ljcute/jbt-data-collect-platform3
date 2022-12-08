bf12e25d14d2b66f35e4e8a29a44ab3ecae9d62c
yanpan01 <gZQVfSe47diV>
2022-12-08 16:37:32 +0800

    新追加海通证券，光大证券的数据采集

--- data/ms/securities/gd_securities_collect.py
+++ data/ms/securities/gd_securities_collect.py
@@ -3,18 +3,15 @@
 # author yanpan
 # 2022/07/06 15:16
 # 光大证券
-
 import os
 import sys
+import time
+import pandas as pd
+from selenium.webdriver.common.by import By
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 sys.path.append(BASE_DIR)
-
-from data.ms.basehandler import BaseHandler
-import time
-from selenium.common import StaleElementReferenceException
-from selenium.webdriver.common.by import By
-from utils.logs_utils import logger
+from data.ms.basehandler import BaseHandler, logger, argv_param_invoke
 
 
 class CollectHandler(BaseHandler):
@@ -23,107 +20,105 @@ class CollectHandler(BaseHandler):
         super().__init__()
         self.mq_msg = os.path.basename(__file__).split('.')[0]
         self.data_source = '光大证券'
+        self.total_page = 0
         self.url = 'http://www.ebscn.com/ourBusiness/xyyw/rzrq/cyxx/'
 
     def rzrq_underlying_securities_collect(self):
-        self.data_list = []
-        self.title_list = ['market', 'sec_code', 'sec_name', 'financing_target', 'securities_mark', 'date']
-        driver = self.get_driver()
-        driver.get(self.url)
-
-        # 第1页内容
-        first_page_content = driver.find_elements(By.XPATH, '//*[@id="bdzq"]/div[2]/table/tbody/tr')[1:]
-        logger.info("光大标的券第1页，共10条")
-        self.resolve_every_page_bd(first_page_content, self.data_list)
-
-        # 找到总页数
-        total_page = 0
-        li_elements = driver.find_elements(By.XPATH, '//*[@id="pageCount"]')
-        if len(li_elements) > 0:
-            total_page = ((li_elements[len(li_elements) - 2].text).split(' ')[1])[0:3]
-
-        for_count = int(total_page) + 1  # range不包括后者
-        for current_page in range(2, for_count):
-            driver.find_elements(By.XPATH, '//*[@id="next_page"]')[0].click()
-            time.sleep(0.5)
-            try:
-                this_page_content = driver.find_elements(By.XPATH, '//*[@id="bdzq"]/div[2]/table/tbody/tr')[1:]
-            except StaleElementReferenceException as es:
-                print(u'查找元素异常%s' % es)
-                print(u'重新获取元素')
-                this_page_content = driver.find_elements(By.XPATH, '//*[@id="bdzq"]/div[2]/table/tbody/tr')[1:]
-                # 处理第[2, total_page]页html
-                # html_content = str(driver.page_source)
-            logger.info("光大标的券第{}页，共10条".format(current_page))
-            self.resolve_every_page_bd(this_page_content, self.data_list)
-            self.collect_num = int(len(self.data_list))
-            self.total_num = int(len(self.data_list))
-
-    def resolve_every_page_bd(self, this_page_content, data_list):
-        if this_page_content:
-            for i in this_page_content:
-                row_list = []
-                row_list.append((i.text).replace(' ', ','))
-                temp_str = row_list[0]
-                new_list = temp_str.split(',')
-                if len(new_list) > 6:
-                    another_list = new_list[2:(len(new_list) - 3)]
-                    del new_list[2:(len(new_list) - 3)]
-                    temp_str = ''.join(another_list)
-                    new_list.insert(2, temp_str)
-                data_list.append(new_list)
+        self._securities_collect('bd')
 
     def guaranty_securities_collect(self):
-        self.data_list = []
-        self.title_list = ['market', 'sec_code', 'sec_name', 'round_rate', 'date']
-        driver = self.get_driver()
-        driver.get(self.url)
+        self._securities_collect('db')
 
-        # 第1页内容
-        first_page_content = driver.find_elements(By.XPATH, '//*[@id="bzj"]/div[2]/table/tbody/tr')[1:]
-        logger.info("光大可充抵保证金券第1页，共10条")
-        self.resolve_every_page_bzj(first_page_content, self.data_list)
+    def _securities_collect(self, biz_type):
+        global next_page_button_element, _df1, _df2
+        try:
+            driver = self.get_driver()
+            driver.get(self.url)
+            driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
+            time.sleep(3)
+            # 当前网页内容(第1页)
+            html_content = str(driver.page_source)
+            if biz_type == 'bd':
+                _df1 = pd.read_html(html_content)[0]
+                _df1.sort_values(by=['证券市场', '证券代码', '证券简称'], ascending=[True, True, True])
+                self.tmp_df = pd.concat([self.tmp_df, _df1])
+                self.collect_num = self.tmp_df.index.size
+            elif biz_type =='db':
+                _df1 = pd.read_html(html_content)[1]
+                _df1.sort_values(by=['证券市场', '证券代码', '证券简称'], ascending=[True, True, True])
+                self.tmp_df = pd.concat([self.tmp_df, _df1])
+                self.collect_num = self.tmp_df.index.size
 
-        # 找到总页数
-        total_page = 0
-        li_elements = driver.find_elements(By.XPATH, '//*[@id="pageCount"]')
-        if len(li_elements) > 0:
-            total_page = ((li_elements[len(li_elements) - 1].text).split(' ')[1])[0:3]
+            # 找到总页数
+            li_elements = driver.find_elements(By.XPATH, '//*[@id="pageCount"]')
+            if len(li_elements) > 0:
+                if biz_type == 'bd':
+                    self.total_page = ((li_elements[len(li_elements) - 2].text).split(' ')[1])[0:3]
+                elif biz_type == 'db':
+                    self.total_page = ((li_elements[len(li_elements) - 1].text).split(' ')[1])[0:3]
 
-        for_count = int(total_page) + 1  # range不包括后者
-        for current_page in range(2, for_count):
-            driver.find_elements(By.XPATH, '//*[@id="next_page"]')[1].click()
-            time.sleep(0.5)
-            try:
-                this_page_content = driver.find_elements(By.XPATH, '//*[@id="bzj"]/div[2]/table/tbody/tr')[1:]
-            except StaleElementReferenceException as es:
-                print(u'查找元素异常%s' % es)
-                print(u'重新获取元素')
-                this_page_content = driver.find_elements(By.XPATH, '//*[@id="bzj"]/div[2]/table/tbody/tr')[1:]
-                # 处理第[2, total_page]页html
-                # html_content = str(driver.page_source)
-            logger.info("光大可充抵保证金券第{}页，共10条".format(current_page))
-            self.resolve_every_page_bzj(this_page_content, self.data_list)
-            self.collect_num = int(len(self.data_list))
-            self.total_num = int(len(self.data_list))
+            current_page = 1
+            logger.info(f"光大证券标的券第{current_page}/{self.total_page}页，记录数{self.collect_num}条")
+            while current_page < int(self.total_page):
+                # 找到下一页 >按钮
+                driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
+                driver.implicitly_wait(10)
+                elements = driver.find_elements(By.XPATH, '//*[@id="next_page"]')
+                driver.implicitly_wait(10)
+                if biz_type == 'bd':
+                    next_page_button_element = elements[0]
+                elif biz_type == 'db':
+                    next_page_button_element = elements[1]
 
-
-    def resolve_every_page_bzj(self, this_page_content, data_list):
-        if this_page_content:
-            for i in this_page_content:
-                row_list = []
-                row_list.append((i.text).replace(' ', ','))
-                temp_str = row_list[0]
-                new_list = temp_str.split(',')
-                if len(new_list) > 5:
-                    another_list = new_list[2:(len(new_list) - 2)]
-                    del new_list[2:(len(new_list) - 2)]
-                    temp_str = ''.join(another_list)
-                    new_list.insert(2, temp_str)
-                data_list.append(new_list)
+                if not next_page_button_element.is_selected():
+                    driver.execute_script('arguments[0].click();', next_page_button_element)
+                    driver.implicitly_wait(10)
+                    # 处理第[2, total_page]页html
+                    if biz_type == 'bd':
+                        _df2 = pd.read_html(html_content)[0]
+                        _df2.sort_values(by=['证券市场', '证券代码', '证券简称'], ascending=[True, True, True])
+                    elif biz_type == 'db':
+                        _df2 = pd.read_html(html_content)[1]
+                        _df2.sort_values(by=['证券市场', '证券代码', '证券简称'], ascending=[True, True, True])
+                    ##############
+                    # 读取内容与上一次完全相同，则休息一下再重新读取内容
+                    counter = 0
+                    while _df1.equals(_df2):
+                        logger.info(f'光大证券标的券采集速度过快，休息一下{counter}')
+                        time.sleep(0.01)
+                        html_content = str(driver.page_source)
+                        if biz_type == 'bd':
+                            _df2 = pd.read_html(html_content)[0]
+                            _df2.sort_values(by=['证券市场', '证券代码', '证券简称'], ascending=[True, True, True])
+                        elif biz_type == 'db':
+                            _df2 = pd.read_html(html_content)[1]
+                            _df2.sort_values(by=['证券市场', '证券代码', '证券简称'], ascending=[True, True, True])
+                        if counter > 100:
+                            driver.quit()
+                            msg = f"光大证券标的券采集速度过快,连续休息100次重试还是存在问题!"
+                            logger.info(msg)
+                            raise Exception(msg)
+                        counter += 1
+                    _df1 = _df2
+                    self.tmp_df = pd.concat([self.tmp_df, _df1])
+                    # 与上一次存在重复，否则需要整个重采
+                    if self.tmp_df.duplicated(['证券市场', '证券代码', '证券简称']).sum() > 0:
+                        dep_line = self.tmp_df[
+                            self.tmp_df.duplicated(['证券市场', '证券代码', '证券简称'], keep='last')]  # 查看删除重复的行
+                        dep_list = dep_line.values.tolist()
+                        msg = f'光大证券标的券采集的重复数据为：{dep_list},共{len(dep_list)}条'
+                        logger.info(msg)
+                        raise Exception(msg)
+                    self.collect_num = self.tmp_df.index.size
+                    logger.info(
+                        f"光大证券标的券第{current_page + 1}/{self.total_page}页，记录数{self.collect_num}/{self.total_num}条")
+                current_page += 1
+            self.data_text = self.tmp_df.to_csv(index=False)
+            if self.collect_num > (int(self.total_page) - 1) * 10:
+                self.total_num = self.collect_num
+        finally:
+            driver.quit()
 
 
 if __name__ == '__main__':
-    # CollectHandler().collect_data(eval(sys.argv[1]))
-    # CollectHandler().collect_data(2)
-    pass
+    argv_param_invoke(CollectHandler(), (2, 3), sys.argv)
--- data/ms/securities/hait_securities_collect.py
+++ data/ms/securities/hait_securities_collect.py
@@ -6,16 +6,21 @@
 # @File    : hait_securities_collect.py
 # @Software: PyCharm
 # 海通证券
+import math
 import os
 import sys
+import time
+
+import pandas as pd
+import json
+import datetime
+import concurrent.futures
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 sys.path.append(BASE_DIR)
 
-from data.ms.basehandler import BaseHandler, get_proxies
-import json
+from data.ms.basehandler import BaseHandler, get_headers, logger, argv_param_invoke
 from constants import *
-import datetime
 
 
 class CollectHandler(BaseHandler):
@@ -24,67 +29,82 @@ class CollectHandler(BaseHandler):
         super().__init__()
         self.mq_msg = os.path.basename(__file__).split('.')[0]
         self.data_source = '海通证券'
+        self.page_size = 5000
+        self.page_count = 1
+        self.init_date = None
         self.url = 'https://www.htsec.com/htAPI/ht/rzrq/bdzqList'
+        self._proxies = self.get_proxies()
 
     def rzrq_underlying_securities_collect(self):
-        search_date = self.search_date if self.search_date is not None else datetime.date.today()
-        page_count = 1
-        page_size = 100
-        while True:
-            params = {"pagesize": page_size, "pagecount": page_count,
-                      "searchDate": str(search_date).replace('-', ''),
-                      "searchCode": '', "jiaoyisuo": '', "type": 1}
-
-            response = self.get_response(self.url, 0, get_headers(), params)
-            text = json.loads(response.text)
-            self.total_num = int(text['result']['allSize'])
-            result_list = text['result']['data']
-            for i in result_list:
-                # market = i['marketCode']
-                # sec_code = i['productCode']
-                # sec_name = i['productName']
-                # if_rz = i['ifFinancing']
-                # if_rq = i['ifGuarantee']
-                self.data_list.append(i)
-                self.collect_num = int(len(self.data_list))
-            if self.total_num is not None and self.total_num > page_count * page_size:
-                page_count = page_count + 1
-            else:
-                break
+        self.init_date = self.search_date.strftime('%Y%m%d')
+        self._securities_collect('bd')
 
     def guaranty_securities_collect(self):
-        search_date = self.search_date if self.search_date is not None else datetime.date.today()
-        page_count = 1
-        page_size = 100
-        while True:
-            params = {"pagesize": page_size, "pagecount": page_count,
-                      "searchDate": str(search_date).replace('-', ''),
-                      "searchCode": '', "jiaoyisuo": '', "type": 2}
+        self.init_date = self.search_date.strftime('%Y%m%d')
+        self._securities_collect('db')
 
-            response = self.get_response(self.url, 0, get_headers(), params)
-            text = json.loads(response.text)
-            self.total_num = int(text['result']['allSize'])
-            result_list = text['result']['data']
-            for i in result_list:
-                # market = i['marketCode']
-                # sec_code = i['productCode']
-                # sec_name = i['productName']
-                # rate = i['stockConvertRate']
-                self.data_list.append(i)
-                self.collect_num = int(len(self.data_list))
-            if self.total_num is not None and self.total_num > page_count * page_size:
-                page_count = page_count + 1
-            else:
-                break
+    def _get_params(self, biz_type, target_page):
+        params = {"pagesize": self.page_size, "pagecount": target_page,
+                  "searchDate": str(self.search_date).replace('-', ''),
+                  "searchCode": '', "jiaoyisuo": ''}
+        if biz_type == 'bd':
+            params["type"] = 1
+        elif biz_type == 'db':
+            params["type"] = 2
+        return params
+
+    def _securities_collect(self, biz_type):
+        params = self._get_params(biz_type, 1)
+        response = self.get_response(self.url, 0, get_headers(), params)
+        text = json.loads(response.text)
+        self.total_num = int(text['result']['allSize'])
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+            self.total_page = math.ceil(self.total_num / self.page_size)
+            step = 5
+            for _page in range(1, self.total_page + 1, step):
+                future_list = []
+                for _i in range(0, step):
+                    if _page + _i > self.total_page:
+                        break
+                    future = executor.submit(self.collect_by_page, biz_type, _page + _i)
+                    future_list.append(future)
+                for r in future_list:
+                    __page, df = r.result()
+                    while df.empty:
+                        try:
+                            self.refresh_proxies(self._proxies)
+                            __page, df = self.collect_by_page(biz_type, __page)
+                        except Exception as e:
+                            time.sleep(1)
+                    logger.info(f" end target_page = {__page}/{self.total_page}, df_size: {df.index.size}")
+                    self.tmp_df = pd.concat([self.tmp_df, df])
+        self.collect_num = self.tmp_df.index.size
+        self.data_text = self.tmp_df.to_csv(index=False)
+
+    def collect_by_page(self, biz_type, target_page):
+        retry_count = 5
+        params = self._get_params(biz_type, target_page)
+        logger.info(f" start target_page = {target_page}/{self.total_page}, params: {params}")
+        while retry_count:
+            try:
+                if retry_count < 5:
+                    self.refresh_proxies(_proxies)
+                _proxies = self._proxies
+                response = requests.get(url=self.url, params=params, headers=get_headers(), proxies=_proxies,
+                                        timeout=120)
+                if response is None or response.status_code != 200:
+                    raise Exception(
+                        f'{self.data_source}数据采集任务请求响应获取异常,已获取代理ip为:{self._proxies}，请求url为:{self.url},请求参数为:{params}')
+                text = json.loads(response.text)
+                result = text['result']['data']
+                return target_page, pd.DataFrame(result)
+            except Exception as e:
+                retry_count -= 1
+                time.sleep(5)
+                self.refresh_proxies(_proxies)
+        return target_page, pd.DataFrame()
+
 
 if __name__ == '__main__':
-    # collector = CollectHandler()
-    # collector.collect_data(3)
-    # if len(sys.argv) > 2:
-    #     collector.collect_data(eval(sys.argv[1]), sys.argv[2])
-    # elif len(sys.argv) == 2:
-    #     collector.collect_data(eval(sys.argv[1]))
-    # elif len(sys.argv) < 2:
-    #     raise Exception(f'business_type为必输参数')
-    # collector.collect_data(2)
-    pass
+    argv_param_invoke(CollectHandler(), (2, 3), sys.argv)
