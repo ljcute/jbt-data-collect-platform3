df5473cea0a93cfa5c32a6fdb085381324700549
yanpan01 <gZQVfSe47diV>
2022-12-14 11:13:38 +0800

    深交所，上交所，交易总量交易明细数据采集入库

--- data/ms/sh/sh_market_mt_trading_collect.py
+++ data/ms/sh/sh_market_mt_trading_collect.py
@@ -10,6 +10,8 @@ import time
 import xlrd2
 import datetime
 import os
+import warnings
+import pandas as pd
 from configparser import ConfigParser
 from selenium.webdriver.common.by import By
 
@@ -18,7 +20,7 @@ sys.path.append(BASE_DIR)
 
 from utils.exceptions_utils import ProxyTimeOutEx
 from utils.logs_utils import logger
-from data.ms.basehandler import BaseHandler
+from data.ms.basehandler import BaseHandler, random_double, argv_param_invoke
 from constants import get_headers
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
@@ -32,13 +34,13 @@ paths = cf.get('excel-path', 'save_excel_file_path')
 save_excel_file_path = os.path.join(paths, "上交所融资融券{}.xls".format(datetime.date.today()))
 
 
-
 class CollectHandler(BaseHandler):
 
     def __init__(self):
         super().__init__()
         self.mq_msg = os.path.basename(__file__).split('.')[0]
         self.data_source = '上海交易所'
+        self.collect_num_check = False
 
     def trading_amount_collect(self):
         trade_date = self.get_trade_date()
@@ -49,10 +51,15 @@ class CollectHandler(BaseHandler):
         self.url = download_excel_url
         response = self.get_response(self.url, 0, get_headers())
         download_excel(response)
-        excel_file = xlrd2.open_workbook(excel_file_path, encoding_override="utf-8")
-        self.data_list, total_row = handle_excel_total(excel_file, trade_date)
-        self.total_num = total_row - 17
-        self.collect_num = len(self.data_list)
+
+        warnings.filterwarnings('ignore')
+        self.tmp_df = pd.read_excel(response.content, header=0, sheet_name='汇总信息')
+        if not self.tmp_df.empty:
+            self.total_num = self.tmp_df.index.size
+            self.collect_num = self.total_num
+            self.data_text = self.tmp_df.to_csv(index=False)
+        else:
+            self.data_status = 3
 
     def trading_items_collect(self):
         trade_date = self.get_trade_date()
@@ -62,10 +69,14 @@ class CollectHandler(BaseHandler):
         self.url = download_excel_url
         response = self.get_response(self.url, 0)
         download_excel(response)
-        excel_file = xlrd2.open_workbook(excel_file_path, encoding_override="utf-8")
-        self.data_list, total_row = handle_excel_detail(excel_file, trade_date)
-        self.total_num = total_row - 1
-        self.collect_num = len(self.data_list)
+        warnings.filterwarnings('ignore')
+        self.tmp_df = pd.read_excel(response.content, header=0, sheet_name='明细信息')
+        if not self.tmp_df.empty:
+            self.total_num = self.tmp_df.index.size
+            self.collect_num = self.total_num
+            self.data_text = self.tmp_df.to_csv(index=False)
+        else:
+            self.data_status = 3
 
     def get_trade_date(self):
         try:
@@ -74,7 +85,9 @@ class CollectHandler(BaseHandler):
             url = 'http://www.sse.com.cn/market/othersdata/margin/detail/'
             driver.get(url)
             time.sleep(3)
-            trade_date = driver.find_elements(By.XPATH, '/html/body/div[8]/div/div[2]/div/div[1]/div[1]/div[1]/table/tbody/tr/td[1]')[0].text
+            trade_date = driver.find_elements(By.XPATH,
+                                              '/html/body/div[9]/div/div[2]/div/div[1]/div[1]/div[1]/table/tbody/tr/td[1]')[
+                0].text
             logger.info(f'上海交易所最新交易日日期为{trade_date}')
             return trade_date
         except ProxyTimeOutEx as es:
@@ -96,67 +109,5 @@ def download_excel(response, query_date=None):
         logger.info("上交所该日无数据:txt_date:{}".format(query_date))
 
 
-# 汇总信息
-def handle_excel_total(excel_file, date):
-    logger.info("开始处理excel")
-    url = 'http://www.sse.com.cn/market/othersdata/margin/sum/'
-    start_dt = datetime.datetime.now()
-    sheet_0 = excel_file.sheet_by_index(0)
-    total_row = sheet_0.nrows
-    try:
-        data_list = []
-        for i in range(1, 2):  # 从第2行开始遍历
-            row = sheet_0.row(i)
-            if row is None:
-                break
-            date = str(date).format("'%Y%m%d'").replace('-', '')  # 信用交易日期
-            rzye = float(str(row[0].value).replace(",", ""))  # 融资余额（元）
-            rzmre = float(str(row[1].value).replace(",", ""))  # 融资买入额（元）
-            rjyl = str(row[2].value).replace(",", "")  # 融券余量
-            rjylje = float(str(row[3].value).replace(",", ""))  # 融券余量金额(元)
-            rjmcl = str(row[4].value).replace(",", "")  # 融券卖出量
-            rzrjye = float(str(row[5].value).replace(",", ""))  # 融资融券余额(元)
-            data_list.append((date, rzye, rzmre, rjyl, rjylje, rjmcl, rzrjye))
-
-        logger.info("excel处理完成，开始处理汇总数据")
-        return data_list, total_row
-    except Exception as e:
-        raise Exception(e)
-
-
-# 详细信息
-def handle_excel_detail(excel_file, date):
-    logger.info("开始解析excel，处理数据！")
-    url = 'http://www.sse.com.cn/market/othersdata/margin/sum/'
-    start_dt = datetime.datetime.now()
-    sheet_1 = excel_file.sheet_by_index(1)
-    total_row = sheet_1.nrows
-    try:
-        data_list = []
-        for i in range(1, total_row):  # 从第2行开始遍历
-            row = sheet_1.row(i)
-            if row is None:
-                break
-            date = str(date).format("'%Y%m%d'").replace('-', '')  # 信用交易日期
-            bdzjdm = str(row[0].value).replace(",", "")  # 标的证劵代码
-            bdzjjc = str(row[1].value).replace(",", "")  # 标的证劵简称
-            rzye = float(str(row[2].value).replace(",", ""))  # 融资余额(元)
-            rzmre = float(str(row[3].value).replace(",", ""))  # 融资买入额(元)
-            rzche = float(str(row[4].value).replace(",", ""))  # 融资偿还额(元)
-            rjyl = str(row[5].value).replace(",", "")  # 融券余量
-            rjmcl = str(row[6].value).replace(",", "")  # 融券卖出量
-            rjchl = str(row[7].value).replace(",", "")  # 融资偿还量
-            data_list.append((date, bdzjdm, bdzjjc, rzye, rzmre, rzche, rjyl, rjmcl, rjchl))
-
-        logger.info("excel处理完成，开始处理详细数据")
-        return data_list, total_row
-    except Exception as e:
-        raise Exception(e)
-
-
-if __name__ == '__main__':
-    collector = CollectHandler()
-    if len(sys.argv) > 1:
-        collector.collect_data(eval(sys.argv[1]))
-    else:
-        logger.error(f'business_type为必传参数')
+if __name__ == "__main__":
+    argv_param_invoke(CollectHandler(), (0, 1), sys.argv)
--- data/ms/sz/sz_market_mt_trading_collect.py
+++ data/ms/sz/sz_market_mt_trading_collect.py
@@ -7,21 +7,19 @@
 import os
 import sys
 import time
-import traceback
+import warnings
 from configparser import ConfigParser
 
+import pandas as pd
 from selenium.webdriver.common.by import By
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 sys.path.append(BASE_DIR)
 
 from utils.exceptions_utils import ProxyTimeOutEx
-from data.ms.basehandler import BaseHandler
-from utils.deal_date import ComplexEncoder
+from data.ms.basehandler import BaseHandler, random_double, argv_param_invoke
 from utils.remove_file import remove_file, random_double
-import json
 import datetime
-import xlrd2
 from constants import USER_AGENTS
 import random
 import os
@@ -46,6 +44,7 @@ class CollectHandler(BaseHandler):
         super().__init__()
         self.mq_msg = os.path.basename(__file__).split('.')[0]
         self.data_source = '深圳交易所'
+        self.collect_num_check = False
 
     def trading_amount_collect(self):
         trade_date = self.get_trade_date()
@@ -59,51 +58,15 @@ class CollectHandler(BaseHandler):
             'random': random_double(),
         }
         response = self.get_response(self.url, 0, headers, params)
-        self.data_list, total_num = self.total_deal(response, trade_date)
-        self.total_num = total_num - 1
-        self.collect_num = len(self.data_list)
-
-    def total_deal(self, response, actual_date):
-        try:
-            try:
-                logger.info("开始下载excel")
-                with open(excel_file_path_anthoer, 'wb') as file:
-                    file.write(response.content)
-                with open(save_excel_file_path_total, 'wb') as file:
-                    file.write(response.content)
-                logger.info("excel下载完成")
-            except Exception as e:
-                raise Exception(e)
-
-            excel_file = xlrd2.open_workbook(excel_file_path_anthoer, encoding_override="utf-8")
-            data_list, total = self.handle_excel_total(excel_file, actual_date)
-            return data_list, total
-        except Exception as e:
-            raise Exception(e)
-        finally:
-            remove_file(excel_file_path_anthoer)
-
-    def handle_excel_total(self, excel_file, actual_date):
-        logger.info("开始处理excel")
-        sheet_0 = excel_file.sheet_by_index(0)
-        total_row = sheet_0.nrows
-        data_list = []
-        if total_row >= 1:
-            for i in range(1, total_row):
-                row = sheet_0.row(i)
-                if row is None:
-                    break
-
-                jrrzye = str(row[1].value).replace(",", "")  # 融资余额(亿元)
-                jrrjye = str(row[4].value).replace(",", "")  # 融券余额(亿元)
-                jrrzrjye = str(row[5].value).replace(",", "")  # 融资融券余额(亿元)
-                jrrzmr = str(row[0].value).replace(",", "")  # 融资买入额(亿元)
-                jrrjmc = str(row[2].value).replace(",", "")  # 融券卖出量(亿股/亿份)
-                jrrjyl = str(row[3].value).replace(",", "")  # 融券余量(亿股/亿份)
-                data_list.append((jrrzye, jrrjye, jrrzrjye, jrrzmr, jrrjmc, jrrjyl))
-
-            logger.info("excel处理结束")
-            return data_list, total_row
+        download_excel(response)
+        warnings.filterwarnings('ignore')
+        self.tmp_df = pd.read_excel(response.content, header=0)
+        if not self.tmp_df.empty:
+            self.total_num = self.tmp_df.index.size
+            self.collect_num = self.total_num
+            self.data_text = self.tmp_df.to_csv(index=False)
+        else:
+            self.data_status = 3
 
     def trading_items_collect(self):
         trade_date = self.get_trade_date()
@@ -120,28 +83,15 @@ class CollectHandler(BaseHandler):
             'TABKEY': 'tab2'
         }
         response = self.get_response(self.url, 0, headers, params)
-        self.data_list, total_num = self.item_deal(response, trade_date)
-        self.collect_num = self.total_num = len(self.data_list)
-
-    def item_deal(self, response, actual_date):
-        try:
-            try:
-                logger.info("开始下载excel")
-                with open(excel_file_path, 'wb') as file:
-                    file.write(response.content)
-                with open(save_excel_file_path, 'wb') as file:
-                    file.write(response.content)
-                logger.info("excel下载完成")
-            except Exception as e:
-                raise Exception(e)
-
-            excel_file = xlrd2.open_workbook(excel_file_path, encoding_override="utf-8")
-            data_list, total_row = self.handle_excel(excel_file, actual_date)
-            return data_list, total_row
-        except Exception as e:
-            raise Exception(e)
-        finally:
-            remove_file(excel_file_path)
+        download_excel(response)
+        warnings.filterwarnings('ignore')
+        self.tmp_df = pd.read_excel(response.content, header=0)
+        if not self.tmp_df.empty:
+            self.total_num = self.tmp_df.index.size
+            self.collect_num = self.total_num
+            self.data_text = self.tmp_df.to_csv(index=False)
+        else:
+            self.data_status = 3
 
     def get_trade_date(self):
         url = 'http://www.szse.cn/disclosure/margin/margin/index.html'
@@ -162,35 +112,19 @@ class CollectHandler(BaseHandler):
         except Exception as e:
             raise Exception(f'获取深圳交易所最新交易日日期异常，请求url为：{url}，具体异常信息为：{e}')
 
-    def handle_excel(self, excel_file, actual_date):
-        logger.info("开始处理excel")
-        sheet_0 = excel_file.sheet_by_index(0)
-        total_row = sheet_0.nrows
-        if total_row >= 1:
-            # try:
-            data_list = []
-            for i in range(1, total_row):  # 从第2行开始遍历
-                row = sheet_0.row(i)
-                if row is None:
-                    break
 
-                zqdm = str(row[0].value).replace(",", "")  # 证券代码
-                zqjc = str(row[1].value).replace(",", "")  # 证券简称
-                jrrzye = str(row[3].value).replace(",", "")  # 融资余额(元)
-                jrrzmr = str(row[2].value).replace(",", "")  # 融资买入额(元)
-                jrrjyl = str(row[5].value).replace(",", "")  # 融券余量(股/份)
-                jrrjye = str(row[6].value).replace(",", "")  # 融券余额(元)
-                jrrjmc = str(row[4].value).replace(",", "")  # 融券卖出量(股/份)
-                jrrzrjye = str(row[7].value).replace(",", "")  # 融资融券余额(亿元)
-                data_list.append((zqdm, zqjc, jrrzye, jrrzmr, jrrjyl, jrrjye, jrrjmc, jrrzrjye))
-
-            logger.info("excel处理结束")
-            return data_list, total_row
+def download_excel(response, query_date=None):
+    if response.status_code == 200:
+        try:
+            with open(excel_file_path, 'wb') as file:
+                file.write(response.content)  # 写excel到当前目录
+            with open(save_excel_file_path, 'wb') as file:
+                file.write(response.content)  # 写excel到当前目录
+        except Exception as es:
+            raise Exception(es)
+    else:
+        logger.info("深交所该日无数据:txt_date:{}".format(query_date))
 
 
 if __name__ == "__main__":
-    collector = CollectHandler()
-    if len(sys.argv) > 1:
-        collector.collect_data(eval(sys.argv[1]))
-    else:
-        logger.error(f'business_type为必传参数')
+    argv_param_invoke(CollectHandler(), (0, 1), sys.argv)
